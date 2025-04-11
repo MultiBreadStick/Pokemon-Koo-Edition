e08e09ff952b7c0e3c68ad6f300d464ec1c456ab
BradNguyen619 <143217357+MultiBreadStick@users.noreply.github.com>
2025-04-10 23:06:54 -0700

    max verstappen

--- moves/move.java
+++ moves/move.java
@@ -1,4 +1,44 @@
 package moves;
-public class move {
+
+public class Move { 
     
+    String name = ""; 
+    String type = ""; 
+    double power = 0; 
+    double accuracy = 0; 
+    double priority = 0; 
+    String category = ""; 
+    String effect = ""; 
+    
+    public Move(String name, String type, double power, double accuracy, double priority, String category, String effect) {
+        this.name = name;
+        this.type = type;
+        this.power = power;
+        this.accuracy = accuracy;
+        this.priority = priority;
+        this.category = category;
+        this.effect = effect;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public String getType() {
+        return type;
+    }
+    
+    public double getPower() {
+        return power;
+    }
+    
+    public double getAccuracy() {
+        return accuracy;
+    }
+    
+    public double getPriority() {
+        return priority;
+    }
 }
+    
+
